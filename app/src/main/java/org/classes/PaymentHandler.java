af9620ef175a9c8533d92a4d2783d43e1e199583
Gabriele Gatti <gabrielegatti432@gmail.com>
2024-11-13 20:43:39 +0100

    Implementation of PaymentHandler class.

--- app/src/main/java/org/classes/PaymentHandler.java
+++ app/src/main/java/org/classes/PaymentHandler.java
@@ -1,5 +1,64 @@
 package org.classes;
 
-public class PaymentHandler {
+import java.util.ArrayList;
+import java.util.List;
+
+import org.interfaces.Handler;
+
+public class PaymentHandler implements Handler {
+    private final List<Order> ordersToPay;
+    private Handler nextHandler;
+    
+    private PaymentHandler() {
+        ordersToPay = new ArrayList<>();
+    }
+
+    private static class PaymentHandlerHelper {
+        private static final PaymentHandler INSTANCE = new PaymentHandler();
+    }
+
+    public static PaymentHandler getInstance() {
+        return PaymentHandlerHelper.INSTANCE;
+    }
+
+    @Override
+    public void setNextHandler(Handler nextHandler) {
+        this.nextHandler = nextHandler;
+    }
+
+    @Override
+    public void handleOrder(Order order) {
+        if (!ordersToPay.contains(order)) {
+            ordersToPay.add(order);
+        }
+    }
+
+    @Override
+    public boolean isReady(Order order) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isReady'");
+    }
+
+    private int calculateCost(Order order) {
+        return new ActualPizza(order.getToppings()).getPrice();
+    }
+
+    public int costPerCustomer(Customer cust) {
+        int total = 0;
+        for (Order order : ordersToPay) {
+            if (order.getCustomer().getName().equals(cust.getName())) {
+                total += calculateCost(order);
+            }
+        }
+        return total;
+    }
+
+    public int totalCost() {
+        int total = 0;
+        for (Order order : ordersToPay) {
+            total += calculateCost(order);
+        }
+        return total;
+    }
 
 }
